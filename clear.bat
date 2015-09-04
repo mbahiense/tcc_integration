04d34ea9da5cae9d9eca2cbe2aaefe44245d8fd9
Marcos <marcos.junior@fpf.br>
2015-09-04 12:38:20 -0400

    Modificando o template e adicionando melhorias

---
+++ clear.bat
@@ -0,0 +1,20 @@
+rem #######################################################################################
+rem Scrip criar os arquivos .bb para posterior conversão de DVI para PDF. (Boundary Box)
+rem #######################################################################################
+rem Necessário modificar o pacote dvips para dvipdfm (pacotes.tex)
+rem Para realizar isso no TexStudio -> Options -> Configure -> Command ->  Build & View -> "DVI - PDF Chain"
+
+
+
+del *.aux
+del *.brf
+del *.dvi
+del *.idx
+del *.lof
+del *.log
+del *.lot
+del *.out.ps
+del *.toc
+del *.bbl
+del *.blg
+del *.pdf
